a7f93840d1d4e97f7b82c4f0330447f60eb236b0
orekyuu <orekyuu@yahoo.co.jp>
2016-08-04 18:06:38 +0900

    first commit

---
+++ src/test/java/net/orekyuu/workbench/Workbench2ApplicationTests.java
@@ -0,0 +1,16 @@
+package net.orekyuu.workbench;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Workbench2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
