bceaa143adbd864cddb722a3f1ccb0e8eefd6f55
orekyuu <orekyuu@gmail.com>
2016-08-15 17:49:02 +0900

    不要なインポートを削除

--- src/main/java/net/orekyuu/workbench/config/Workbench2WebConfig.java
+++ src/main/java/net/orekyuu/workbench/config/Workbench2WebConfig.java
@@ -5,11 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class Workbench2WebConfig extends WebMvcConfigurerAdapter {
