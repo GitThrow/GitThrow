ab96da1613ada0620f865e176b3eab2c683de1d0
orekyuu <orekyuu@yahoo.co.jp>
2016-08-19 22:19:23 +0900

    プロジェクトが存在しない時にエラーページを出す

--- src/main/java/net/orekyuu/workbench/controller/view/user/project/ProjectControllerAdvice.java
+++ src/main/java/net/orekyuu/workbench/controller/view/user/project/ProjectControllerAdvice.java
@@ -4,11 +4,9 @@ import net.orekyuu.workbench.entity.Project;
 import net.orekyuu.workbench.service.ProjectService;
 import net.orekyuu.workbench.service.exceptions.ProjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice("net.orekyuu.workbench.controller.view.user.project")
 public class ProjectControllerAdvice {
@@ -20,5 +18,12 @@ public class ProjectControllerAdvice {
     public void addProjectObject(@PathVariable("projectId") String projectId, Model model) throws ProjectNotFoundException {
         Project project = projectService.findById(projectId).orElseThrow(() -> new ProjectNotFoundException(projectId));
         model.addAttribute("project", project);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleProjectNotFound(ProjectNotFoundException e, Model model) {
+        model.addAttribute("projectId", e.getProjectId());
+        return "error/project_not_found";
     }
 }
--- src/main/java/net/orekyuu/workbench/service/exceptions/ProjectNotFoundException.java
+++ src/main/java/net/orekyuu/workbench/service/exceptions/ProjectNotFoundException.java
@@ -9,7 +9,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProjectNotFoundException extends Exception {
 
+    private String projectId;
     public ProjectNotFoundException(String projectId) {
         super(projectId);
+        this.projectId = projectId;
+    }
+
+    public String getProjectId() {
+        return projectId;
     }
 }
