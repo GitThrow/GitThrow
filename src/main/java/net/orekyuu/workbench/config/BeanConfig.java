daa2b5a6d2e0f81bad2568b86be0b43ba3be54b6
orekyuu <orekyuu@yahoo.co.jp>
2016-08-20 12:13:20 +0900

    Workbenchのユーザーでベーシック認証できるようにした

--- src/main/java/net/orekyuu/workbench/config/BeanConfig.java
+++ src/main/java/net/orekyuu/workbench/config/BeanConfig.java
@@ -1,8 +1,10 @@
 package net.orekyuu.workbench.config;
 
 import net.orekyuu.workbench.service.ProjectService;
+import net.orekyuu.workbench.service.TicketService;
 import net.orekyuu.workbench.service.UserService;
 import net.orekyuu.workbench.service.impl.ProjectServiceImpl;
+import net.orekyuu.workbench.service.impl.TicketServiceImpl;
 import net.orekyuu.workbench.service.impl.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +20,10 @@ public class BeanConfig {
     @Bean
     public ProjectService projectService() {
         return new ProjectServiceImpl();
+    }
+
+    @Bean
+    public TicketService ticketService() {
+        return new TicketServiceImpl();
     }
 }
